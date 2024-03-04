77529df28ed82d162355d7e68dce67ea42f0eade
iseeyoucopy <iseeyoucopy@yahoo.com>
2024-03-04 13:17:14 +0000

    create ro.lua
    Added Romanian language support

---
+++ languages/ro.lua
@@ -0,0 +1,23 @@
+--------------------------------------------------------------------------------------
+---------------------------------- Romanian ------------------------------------------
+
+Locales["ro_lang"] = {
+    InputHeader = 'Cate {{msg}} doresti sa confectionezi',
+    InputCraft = 'Confectioneaza obiectul',
+    InputCancel = 'Anuleaza',
+    BackButton = 'Inapoi',
+    ExitButton = 'Iesire',
+    CraftHeader = 'Confectionare',
+    CraftText = 'Incepe confectionarea',
+    InvalidAmount = 'Cantitate invalida',
+    Crafting = 'Se confectioneaza...',
+    FinishedCrafting = 'Ai terminat confectionarea',
+    PlaceFire = 'Plasezi un foc de tabara...',
+    PutOutFire = 'Stingi focul de tabara...',
+    NotEnough = 'Nu ai suficiente materiale pentru aceasta reteta',
+    NotJob = 'Nu ai meseria necesara ',
+    TooFull = 'Inventarul este prea plin pentru a confectiona',
+    WeaponsFull = 'Nu poti cara mai multe arme',
+    WebhookItem = 'Ai confectionat obiectul:',
+    WebhookWeapon = 'Ai creat arma:',
+}
